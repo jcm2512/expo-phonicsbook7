cf61710cc1b37193c5c8c450de060954db1acc18
jcm2512 <joe.magalotti@gmail.com>
2022-12-30 00:20:11 +0900

    not working for web

--- Example.js
+++ Example.js
@@ -1,56 +1,58 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 
 import { StyleSheet, View, Text, Image } from "react-native";
 import { ReactNativeZoomableView } from "@openspacelabs/react-native-zoomable-view";
-import { Dimensions } from "react-native";
 
-const zoomableViewRef = React.createRef();
-
-const handleZoomEnd = (event, gestureState, zoomableViewEventObject) => {
-  console.log(zoomableViewEventObject);
-  //   const offsetX =
-  //     zoomableViewEventObject.originalWidth -
-  //     (zoomableViewEventObject.originalWidth - zoomableViewEventObject.offsetX);
-  //   const offsetY =
-  //     zoomableViewEventObject.offsetY - zoomableViewEventObject.originalHeight;
-  //   console.log(zoomableViewEventObject);
-  //   console.log(-offsetX, offsetY);
-  //   if (offsetX < 0) {
-  //     zoomableViewRef.current.moveBy(offsetX, 0);
-  //   }
-};
+import { Asset } from "expo-asset";
 
 export default function App() {
+  const zoomableViewRef = React.createRef();
+
+  const [imageAsset, setImageAsset] = useState(null);
+  const [imgSize, setImgSize] = useState(null);
+
+  useEffect(() => {
+    async function loadImage() {
+      const asset = Asset.fromModule(require("./assets/phonics_1_5.png"));
+      await asset.downloadAsync();
+      setImageAsset(asset);
+      setImgSize(asset);
+      console.log(imgSize);
+    }
+    loadImage();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>ReactNativeZoomableView</Text>
-      <View
-        style={{
-          //   borderWidth: 5,
-          flex: 1,
-          //   flexShrink: 1,
-          height: Dimensions.height,
-          width: Dimensions.width,
-          //   width: "auto",
-        }}
+      <ReactNativeZoomableView
+        ref={zoomableViewRef}
+        maxZoom={20}
+        minZoom={1}
+        bindToBorders={true}
+        style={{ justifyContent: "flex-start" }}
       >
-        <ReactNativeZoomableView
-          ref={zoomableViewRef}
-          maxZoom={20}
-          minZoom={1}
-          bindToBorders={true}
-          //   contentWidth={300}
-          //   contentHeight={150}
-          panBoundaryPadding={0}
-        >
-          <Image
-            style={{ width: "100%", height: "100%", resizeMode: "contain" }}
-            source={require("./assets/phonics_1_5.png")}
-          />
-        </ReactNativeZoomableView>
-        <StatusBar style="auto" />
-      </View>
+        <Image
+          style={{
+            width: "100%",
+            height: "100%",
+            resizeMode: "contain",
+            position: "absolute",
+          }}
+          source={
+            imageAsset ? { uri: imageAsset.localUri || imageAsset.uri } : null
+          }
+        />
+        <View
+          style={{
+            position: "absolute",
+            width: 50,
+            height: 50,
+            backgroundColor: "red",
+          }}
+        ></View>
+      </ReactNativeZoomableView>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -58,13 +60,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "stretch",
-    justifyContent: "center",
-    // padding: 20,
+    // alignItems: "stretch",
+    // justifyContent: "center",
   },
-  //   box: {
-  //     width: 60,
-  //     height: 60,
-  //     marginVertical: 20,
-  //   },
 });
