5567124d5408b6bc8bd9a1c522303ffd279491fe
jcm2512 <joe.magalotti@gmail.com>
2022-12-30 01:51:43 +0900

    set scale to device width

--- Example.js
+++ Example.js
@@ -4,53 +4,80 @@ import { StatusBar } from "expo-status-bar";
 
 import { StyleSheet, View, Text, Image } from "react-native";
 import { ReactNativeZoomableView } from "@openspacelabs/react-native-zoomable-view";
+import { Dimensions } from "react-native";
 
 import { Asset } from "expo-asset";
+
+async function loadImage() {
+  const imageAsset = Asset.fromModule(require("./assets/phonics_1_5.png"));
+  await imageAsset.downloadAsync();
+  return imageAsset;
+}
 
 export default function App() {
   const zoomableViewRef = React.createRef();
 
   const [imageAsset, setImageAsset] = useState(null);
-  const [imgSize, setImgSize] = useState(null);
+  const [imgSize, setImgSize] = useState({ width: 100, height: 100 });
+  const [maxWidth, setMax] = useState(100);
 
   useEffect(() => {
-    async function loadImage() {
-      const asset = Asset.fromModule(require("./assets/phonics_1_5.png"));
-      await asset.downloadAsync();
+    loadImage().then((asset) => {
       setImageAsset(asset);
-      setImgSize(asset);
-      console.log(imgSize);
-    }
-    loadImage();
+      setImgSize({ width: asset.width, height: asset.height });
+    });
   }, []);
+
+  useEffect(() => {
+    setMax(Dimensions.get("window").width / imgSize.width);
+    console.log(maxWidth);
+  }, [imgSize]);
+
   return (
     <View style={styles.container}>
       <ReactNativeZoomableView
         ref={zoomableViewRef}
-        maxZoom={20}
-        minZoom={1}
+        maxZoom={5}
+        minZoom={maxWidth}
         bindToBorders={true}
-        style={{ justifyContent: "flex-start" }}
+        contentHeight={1200}
+        contentWidth={724}
+        style={{
+          padding: 0,
+          // justifyContent: "flex-start",
+          // height: 40,
+          // width: 1200,
+          backgroundColor: "blue",
+        }}
       >
-        <Image
-          style={{
-            width: "100%",
-            height: "100%",
-            resizeMode: "contain",
-            position: "absolute",
-          }}
-          source={
-            imageAsset ? { uri: imageAsset.localUri || imageAsset.uri } : null
-          }
-        />
         <View
           style={{
-            position: "absolute",
-            width: 50,
-            height: 50,
-            backgroundColor: "red",
+            height: 1024,
+            width: 724,
+            backgroundColor: "green",
           }}
-        ></View>
+        >
+          <Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: "contain",
+              position: "absolute",
+            }}
+            source={
+              imageAsset ? { uri: imageAsset.localUri || imageAsset.uri } : null
+            }
+          />
+          <View
+            style={{
+              position: "absolute",
+              width: 24,
+              height: 24,
+              backgroundColor: "red",
+              left: 700,
+            }}
+          ></View>
+        </View>
       </ReactNativeZoomableView>
       <StatusBar style="auto" />
     </View>
@@ -60,7 +87,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: "stretch",
-    // justifyContent: "center",
+    alignItems: "stretch",
+    justifyContent: "center",
   },
 });
